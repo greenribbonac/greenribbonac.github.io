93ba053f55e4c6b28d852425ebaaed116d4fe357
HIMURA Tomohiko a.k.a eiel <eiel.hal@gmail.com>
2025-07-12 01:05:34 +0900

    feat: スポンサーカテゴリ実装

--- components/SponsorList.tsx
+++ components/SponsorList.tsx
@@ -1,9 +1,15 @@
-import type { ReactElement, ReactNode } from "react";
+import { Fragment, type ReactElement, type ReactNode } from "react";
 
-interface SponsorType {
+export interface SponsorType {
   name: string;
   url: string;
 }
+
+export interface SponsorCategory {
+  category: string;
+  sponsors: { name: string; url: string }[];
+}
+
 
 function Sponsor({ name, url }: SponsorType): ReactElement {
   if (!url) {
@@ -21,18 +27,45 @@ function Sponsor({ name, url }: SponsorType): ReactElement {
   );
 }
 
-export function SponsorList({
+function SponsorList({
   sponsors,
 }: {
   sponsors: SponsorType[];
 }): ReactElement {
   return (
-    <ul className="partner__list flexbox flex-justify-between flex-wrap flex-align-center">
+    <>
       {sponsors.map((sponsor) => (
         <li className="partner__item" key={sponsor.name}>
           <Sponsor {...sponsor} />
         </li>
       ))}
-    </ul>
+    </>
   );
+}
+
+interface SpnosorCategory {
+  category: string;
+  sponsors: { name: string; url: string }[];
+}
+
+
+export function SponsorBlock({ sponsorAlist }:{
+  sponsorAlist: SponsorCategory[]
+}): ReactElement {
+  return (
+    <>
+	{sponsorAlist.map(({ category, sponsors }, index) => {
+	  // 協賛団体はヘッダーを表示しない
+	  return (
+	    <Fragment key="category">
+	      {index !== 0 &&
+		(<><hr className="dotted-devider" />
+		   <p className="txt--yugo18 t-center animation-bottom">{category}</p></>)}
+              <ul className="partner__list flexbox flex-justify-between flex-wrap flex-align-center">
+		<SponsorList sponsors={sponsors} />
+              </ul>
+	    </Fragment>
+	  );
+	})}
+    </>)
 }
--- generate/genSponsor.tsx
+++ generate/genSponsor.tsx
@@ -1,10 +1,22 @@
 import { format } from "prettier";
 import { renderToStaticMarkup } from "react-dom/server";
 
-import { SponsorList } from "../components/SponsorList.js";
+import { SponsorBlock, SponsorType, SponsorCategory } from "../components/SponsorList.js";
 
-import sponsors from "../resources/sponsorship.json" assert { type: "json" };
+import sponsorship from "../resources/sponsorship.json" with { type: "json" }
 
-const html = renderToStaticMarkup(<SponsorList sponsors={sponsors} />);
+
+const categories = ["協賛団体", "自動販売機支援団体", "協力団体"];
+const sponsorAlist: SponsorCategory[] = categories.map(category => {
+  return sponsorship.reduce<SponsorCategory>((acc, n) => {
+    if (n.category === category) {
+      acc.sponsors.push({ name: n.name, url: n.url})
+    }
+    return acc;
+  }, { category: category, sponsors: [] });
+});
+
+
+const html = renderToStaticMarkup(<SponsorBlock sponsorAlist={sponsorAlist} />);
 const formatted = await format(html, { parser: "html"});
 console.log(formatted);
